d084ff693e8a9f9d32cf665e4e481990945a6e40
intisarnazim12 <intisarnazim12@gmail.com>
2026-02-23 19:39:16 +0000

    did problem one w/ mr belcher

--- src/main/java/unit1/Unit1.java
+++ src/main/java/unit1/Unit1.java
@@ -17,7 +17,7 @@ public class Unit1 {
   */
  public static int addIntegers(int a, int b) {
      // Your code here
-     return 0; // Placeholder return
+     return a + b; // Placeholder return
  }
 
  /**
@@ -66,5 +66,6 @@ public class Unit1 {
  public static double multiplyIntAndDouble(int a, double b) {
      // Your code here
      return 0.0; // Placeholder return
+     sum = (a+b);
  }
 }
